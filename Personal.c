09dfcfea5174a7d2276b8ca188247a3fa7fe0304
beaalberto <bea_alberto@dlsu.edu.ph>
2016-05-26 16:46:02 +0800

    Create Personal.c

---
+++ Personal.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	int a;
+	
+	printf("\t\t");
+	
+	for(a=1;a<=28;a++)
+	{
+		printf("@");
+	}
+	
+	printf("\n\n\t\t\tBea S. Alberto");
+	printf("\n\n\t\t\tBS CIV");
+	printf("\n\n\t\t\t114");
+	printf("\n\n\t\t\tMay 26,2016");
+	printf("\n\n\t\t\tEmail");
+	printf("\n\n\t\t\tNumber\n\n\t\t");
+	
+	for(a=1;a<=28;a++)
+	{
+		printf("@");
+	}
+	
+	system("PAUSE");
+	return 0;
+	
+}
+
+
