0769425e34826cc76c37b94afd83fae9ba34794f
beaalberto <bea_alberto@dlsu.edu.ph>
2016-05-26 16:46:47 +0800

    Create Triangle.c

---
+++ Triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float ba,he,area;
+	
+	printf("\nInput Base of Triangle: ");
+	scanf("%f",&ba);
+	printf("\nInput Height of Triangle: ");
+	scanf("%f",&he);
+	
+	area=ba*he*1/2;
+	
+	printf("\n\nThe Area of the triangle is:  %.2f",area);
+	
+	getch();
+	return 0;
+}
